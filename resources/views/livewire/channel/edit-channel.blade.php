84280aeee08a9afda43309a42a42b14badfd04e2
Samandar <xkas2001@gmail.com>
2021-07-17 20:45:48 +0500

    Build the edit form

--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,4 +1,26 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
-    {{ $channel->name }}
+
+    <form wire:submit.prevent="update">
+
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea cols="30" rows="4" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+
+    </form>
+
 </div>
